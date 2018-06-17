62a9201ec4fe7e2fdab86d246f3c02cb4ef5ee55
John Marshall <j21marshall@gmail.com>
2018-06-16 22:08:51 -0400

    Add files via upload

--- vihuela_0_1.py
+++ vihuela_0_1.py
@@ -4,6 +4,52 @@ import wave
 
 br = 64000
 
+def genwave(afs,lenwav):
+    outsig = np.zeros(lenwav)
+    for ampfreq in afs:
+        outsig += ampfreq[0]*np.sin((np.arange(lenwav))/((br/ampfreq[1])/np.pi))
+        #outsig += ampfreq[0]*np.sin(np.pi*scal[n+int(len(scal)/2)]*t/br)
+    return outsig
+
+def envwave(inwav):
+    fadein = int(len(inwav)/10)
+    fadeout = int(len(inwav)/5)
+    for n in range(fadein):
+        inwav[n] = n/fadein*inwav[n]
+    for n in range(fadeout):
+        inwav[-n] = n/fadeout*inwav[-n]
+    return inwav
+
+def instr1(infreq):
+    a1 = [(2/3,infreq)]
+    rems = 1/3
+    for n in range(2,7):
+        a1.append((rems/3,infreq*n))
+        a1.append((rems/3,infreq/n))
+        rems = rems/3
+    return a1
+
+def instr2(infreq):
+    a1 = [(1/3,infreq)]
+    a1.append((1/9,infreq+.0003))
+    a1.append((1/9,infreq-.0003))
+    rems = 1/2
+    for n in range(2,10):
+        a1.append((rems/n**2,infreq*n))
+    a1.append((1/8,infreq/2))
+    return a1
+
+def null_instr(infreq):
+    return [(1,infreq)]
+
+def chor1(afs,offset=.003):
+    afso = []
+    for ampfreq in afs:
+        afso.append((ampfreq[0]/3,ampfreq[1]))
+        afso.append((ampfreq[0]/3,ampfreq[1]*(1+offset)))
+        afso.append((ampfreq[0]/3,ampfreq[1]*(1-offset)))
+    return afso
+
 def genedo(edo,base,rnge=3):
     nfreqs = []
     for n in range(-edo*rnge,edo*rnge):
@@ -50,8 +96,9 @@ savefile = input('What file name to save to (e.g. \'wavcomp.wav\')? ')
 signal = ''
 
 for n in melody:
-    for t in range(int(br/4)):
-        signal += chr(int(np.sin(np.pi*scal[n+int(len(scal)/2)]*t/br)*127+128))        
+    abc = envwave(genwave(chor1((instr2(scal[n+int(len(scal)/2)]))),int(br/4)))
+    for k in abc:
+        signal +=  chr(int(k*127+128))
 
 p = pyaudio.PyAudio()
 stream = p.open(format = p.get_format_from_width(1),
@@ -60,7 +107,7 @@ stream = p.open(format = p.get_format_from_width(1),
                 output = True)     
 stream.close()
 p.terminate()
-        
+
 wf = wave.open(savefile, 'wb')
 wf.setnchannels(1)
 wf.setsampwidth(p.get_sample_size(pyaudio.paInt8))
